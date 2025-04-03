9e989b7daebac39359a4ab2b895b5ab9bc61a7de
Soxna Rama Mu Sell Mi <ramatoulayef56@gmail.com>
2025-04-03 21:34:36 +0000

    intialisation node/express

--- gestionRestau.sql
+++ gestionRestau.sql
@@ -8,22 +8,10 @@ CREATE TABLE Utilisateur (
     Prenom VARCHAR(50) NOT NULL,
     Mot_de_passe VARCHAR(255) NOT NULL,
     Email VARCHAR(100) UNIQUE NOT NULL,
+    CONSTRAINT CHK_Email CHECK (Email LIKE '%@ucad.edu.sn'),
     Sexe ENUM('Homme', 'Femme') NOT NULL,
-    dateNaissance DATE NOT NULL
-);
-
--- Table Restaurateur (Hérite de Utilisateur)
-CREATE TABLE Restaurateur (
-    Id_Restaurateur INT PRIMARY KEY AUTO_INCREMENT,
-    Id_Utilisateur INT UNIQUE,
-    FOREIGN KEY (Id_Utilisateur) REFERENCES Utilisateur(Id_Utilisateur) ON DELETE CASCADE
-);
-
--- Table Gérant (Hérite de Utilisateur)
-CREATE TABLE Gerant (
-    Id_Gerant INT PRIMARY KEY AUTO_INCREMENT,
-    Id_Utilisateur INT UNIQUE,
-    FOREIGN KEY (Id_Utilisateur) REFERENCES Utilisateur(Id_Utilisateur) ON DELETE CASCADE
+    dateNaissance DATE NOT NULL,
+    Role ENUM('Restaurateur', 'Gerant', 'Etudiant', 'Agent') NOT NULL
 );
 
 -- Table Ticket
@@ -43,14 +31,6 @@ CREATE TABLE Etudiant (
     FOREIGN KEY (idTicket) REFERENCES Ticket(Id_Ticket)
 );
 
-
--- Table Agent (Hérite de Utilisateur)
-CREATE TABLE Agent (
-    Id_Agent INT PRIMARY KEY AUTO_INCREMENT,
-    Id_Utilisateur INT UNIQUE,
-    FOREIGN KEY (Id_Utilisateur) REFERENCES Utilisateur(Id_Utilisateur) ON DELETE CASCADE
-);
-
 -- Table Restaurant
 CREATE TABLE Restaurant (
     Id_Restaurant INT PRIMARY KEY AUTO_INCREMENT,
@@ -59,19 +39,19 @@ CREATE TABLE Restaurant (
 
 -- Relation entre Gérant et Restaurant (1,n)
 CREATE TABLE GerantResto (
-    Id_Gerant INT,
+    Id_Utilisateur INT,
     Id_Restaurant INT,
-    PRIMARY KEY (Id_Gerant, Id_Restaurant),
-    FOREIGN KEY (Id_Gerant) REFERENCES Gerant(Id_Gerant) ON DELETE CASCADE,
+    PRIMARY KEY (Id_Utilisateur, Id_Restaurant),
+    FOREIGN KEY (Id_Utilisateur) REFERENCES Utilisateur(Id_Utilisateur) ON DELETE CASCADE,
     FOREIGN KEY (Id_Restaurant) REFERENCES Restaurant(Id_Restaurant) ON DELETE CASCADE
 );
 
 -- Relation entre Restaurateur et Restaurant (1,n)
-CREATE TABLE gestionresto (
-    Id_Restaurateur INT,
+CREATE TABLE GestionResto (
+    Id_Utilisateur INT,
     Id_Restaurant INT,
-    PRIMARY KEY (Id_Restaurateur, Id_Restaurant),
-    FOREIGN KEY (Id_Restaurateur) REFERENCES Restaurateur(Id_Restaurateur) ON DELETE CASCADE,
+    PRIMARY KEY (Id_Utilisateur, Id_Restaurant),
+    FOREIGN KEY (Id_Utilisateur) REFERENCES Utilisateur(Id_Utilisateur) ON DELETE CASCADE,
     FOREIGN KEY (Id_Restaurant) REFERENCES Restaurant(Id_Restaurant) ON DELETE CASCADE
 );
 
@@ -86,20 +66,18 @@ CREATE TABLE Menu (
 
 -- Relation entre Restaurateur et Menu (1,n)
 CREATE TABLE RestaurateurMenu (
-    Id_Restaurateur INT,
+    Id_Utilisateur INT,
     Id_Menu INT,
-    PRIMARY KEY (Id_Restaurateur, Id_Menu),
-    FOREIGN KEY (Id_Restaurateur) REFERENCES Restaurateur(Id_Restaurateur) ON DELETE CASCADE,
+    PRIMARY KEY (Id_Utilisateur, Id_Menu),
+    FOREIGN KEY (Id_Utilisateur) REFERENCES Utilisateur(Id_Utilisateur) ON DELETE CASCADE,
     FOREIGN KEY (Id_Menu) REFERENCES Menu(Id_Menu) ON DELETE CASCADE
 );
 
 -- Relation entre Ticket et Restaurant (n,1)
-CREATE TABLE  TicketResto (
+CREATE TABLE TicketResto (
     Id_Ticket INT,
     Id_Restaurant INT,
     PRIMARY KEY (Id_Ticket, Id_Restaurant),
     FOREIGN KEY (Id_Ticket) REFERENCES Ticket(Id_Ticket) ON DELETE CASCADE,
     FOREIGN KEY (Id_Restaurant) REFERENCES Restaurant(Id_Restaurant) ON DELETE CASCADE
 );
-
- 
